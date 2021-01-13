506ccc3d2be52654e8d82af9bd62d16da7a57d48
pau2017 <pau.higon.2017@lacetania.cat>
2021-01-13 20:17:27 +0100

    2on canvi

--- Grua_amb_servo/Grua_amb_servo.ino
+++ Grua_amb_servo/Grua_amb_servo.ino
@@ -14,13 +14,15 @@ void setup()
 void loop() 
 {
  
-  if (analogRead(0) <200  &&  eix<180) {
-  eix++;
-  servo.write(eix);
+  if (analogRead(0) <200  &&  eix<180)
+  {
+    eix++;
+    servo.write(eix);
   }
-  if (analogRead(0)>700 &&  eix>0){
-  eix--;
-  servo.write(eix);
+  if (analogRead(0)>700 &&  eix>0)
+  {
+    eix--;
+    servo.write(eix);
   } 
 
   delay(8);
