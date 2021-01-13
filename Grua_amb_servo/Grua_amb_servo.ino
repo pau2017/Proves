8a49d4d0efee585f0a939587dd24663acb07eb53
pau2017 <pau.higon.2017@lacetania.cat>
2021-01-13 20:14:05 +0100

    1er dia de feina

---
+++ Grua_amb_servo/Grua_amb_servo.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+Servo servo;
+int eix=90;
+
+void setup() 
+{
+  
+  servo.attach(6);
+  servo.write(90);
+  
+}
+
+void loop() 
+{
+ 
+  if (analogRead(0) <200  &&  eix<180) {
+  eix++;
+  servo.write(eix);
+  }
+  if (analogRead(0)>700 &&  eix>0){
+  eix--;
+  servo.write(eix);
+  } 
+
+  delay(8);
+
+}
+ 
